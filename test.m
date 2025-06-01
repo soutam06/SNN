728abca528b072539426ec35434c019aae36e26e
soutam <rajbharsoutam@gmail.com>
2025-06-01 23:37:24 +0530

    Code Run Sucessfully

--- test.m
+++
@@ -1,5 +0,0 @@
-% Run this in command window to inspect problematic stocks
-load('portfolio_data.mat');
-problem_stocks = symbols(unique(col));
-disp('Stocks with extreme returns:');
-disp(problem_stocks');
